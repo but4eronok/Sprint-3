63379f13a64de186a02f14b7c57f2f6603685973
Kirill Sokolinskiy <kirill8256@rambler.ru>
2021-09-11 17:09:04 +0300

    Archivator.kt done

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,24 +1,53 @@
 package ru.sber.io
 
-/**
- * Реализовать методы архивации и разархивации файла.
- * Для реализиации использовать ZipInputStream и ZipOutputStream.
- */
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
+
 class Archivator {
+    val file = File("io\\logfile.log")
+    val zip = File("io\\logfile.zip")
+    val unzippedFile = File("io\\unzipped log file.log")
 
-    /**
-     * Метод, который архивирует файл logfile.log в архив logfile.zip.
-     * Архив должен располагаться в том же каталоге, что и исходной файл.
-     */
     fun zipLogfile() {
-
+        var fis: InputStream? = null
+        var zos: ZipOutputStream? = null
+        try {
+            fis = FileInputStream(file)
+            zos = ZipOutputStream(FileOutputStream(zip))
+            zos.putNextEntry(ZipEntry(file.name))
+            var readByte = fis.read()
+            while (readByte != -1) {
+                zos.write(readByte)
+                readByte = fis.read()
+            }
+        } finally {
+            fis?.close()
+            zos?.close()
+        }
     }
 
-    /**
-     * Метод, который извлекает файл из архива.
-     * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
-     */
     fun unzipLogfile() {
-
+        var zis: ZipInputStream? = null
+        var fos: FileOutputStream? = null
+        try {
+            zis = ZipInputStream(FileInputStream(zip))
+            fos = FileOutputStream(unzippedFile)
+            zis.nextEntry
+            var readByte = zis.read()
+            while (readByte != -1) {
+                fos.write(readByte)
+                readByte = zis.read()
+            }
+        } finally {
+            zis?.close()
+            fos?.close()
+        }
     }
 }
+
