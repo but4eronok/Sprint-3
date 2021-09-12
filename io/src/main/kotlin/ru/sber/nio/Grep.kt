ed904c7a171412cb08062ebf2fd2fa81088e608a
Kirill Sokolinskiy <kirill8256@rambler.ru>
2021-09-12 19:51:58 +0300

    nio done

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,20 +1,37 @@
 package ru.sber.nio
 
+import jdk.incubator.foreign.MemorySegment.READ
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Files
+import java.nio.file.Files.*
+import java.nio.file.Paths
+import java.util.*
+import kotlin.io.path.bufferedWriter
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.name
+import kotlin.io.path.useLines
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
 class Grep {
-    /**
-     * Метод должен выполнить поиск подстроки subString во всех файлах каталога logs.
-     * Каталог logs размещен в данном проекте (io/logs) и внутри содержит другие каталоги.
-     * Результатом работы метода должен быть файл в каталоге io(на том же уровне, что и каталог logs), с названием result.txt.
-     * Формат содержимого файла result.txt следующий:
-     * имя файла, в котором найдена подстрока : номер строки в файле : содержимое найденной строки
-     * Результирующий файл должен содержать данные о найденной подстроке во всех файлах.
-     * Пример для подстроки "22/Jan/2001:14:27:46":
-     * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
-     */
-    fun find(subString: String) {
 
+    fun find(subString: String) {
+        val path = Paths.get("io/logs")
+        val programResult = Paths.get("io/result.txt")
+
+        programResult.bufferedWriter().use { buff ->
+            find(path, 10, { p, _ -> p.toString().endsWith(".log") }).forEach { logs ->
+                logs.useLines { lines ->
+                    lines.filter { currLine ->
+                        currLine.contains(subString)
+                    }.forEach { subString ->
+                        buff.write("${logs.name} : ${logs.useLines { it.indexOf(subString) } + 1} : $subString")
+                    }
+                }
+            }
+        }
     }
 }
